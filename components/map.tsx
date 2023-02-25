8898b3bd74445c8f185019c43dbc7cfb456f8fc3
aitorcfigueroa <aitorcf@gmail.com>
2023-02-25 02:48:24 +0100

    Added: devices on map

--- components/map.tsx
+++ components/map.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Image from "next/image";
 import mapa from "@/public/planox.jpg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -11,20 +11,39 @@ interface IProps {
 }
 
 const Map: React.FC<IProps> = ({ children }) => {
-
+  const [devices, setDevices] = useState<any>();
   const refContainer = useRef<any>(null);
-  const pos = { x: 10, y: 50};
-
+  
+  const fetchData = () => {
+    fetch('http://localhost:3000/api/controllers/devices')
+      .then(response => response.json())
+      .then(json => {
+        setDevices(json)
+      })
+  }
+  
   useEffect(() => {
+    fetchData();
     if (!refContainer || !refContainer.current) { return; }
     refContainer.current.addEventListener('mousemove', (e: any) => {
       const boundary = e.target.getBoundingClientRect();
-      console.log({ 
+      /*console.log({ 
         x: e.clientX - boundary.left, 
         y: e.clientY - boundary.top 
-       });
+      });*/
     });
+
+    
   }, [])
+
+  function deviceIcon(device: any) {
+    if (device === 1) {
+      return faLaptop
+    } else {
+      return faTemperatureHalf
+    }
+  }
+
   return (
     <div className={styles.layout}>
       <div className={styles.iconList}>
@@ -37,29 +56,13 @@ const Map: React.FC<IProps> = ({ children }) => {
       </div>
       <div className={styles.mapContainer}>
           <Image className={styles.map} ref={refContainer} alt="" src={mapa} />
-          <div className={styles.marker} style={{ top: `${pos.x}%`, left: `${pos.y}%`}}>
-            <FontAwesomeIcon icon={faLaptop} />
-          </div>
-        {/* <div className={styles.activeDevices} >
-          <div className={styles.icon} style={{top: '20%', left: '75px'}}>
-            <FontAwesomeIcon icon={faLaptop}/>
-          </div>
-          <div className={styles.icon} style={{top: '80px', left: '235px'}}>
-            <FontAwesomeIcon icon={faLaptop} />
-          </div>
-          <div className={styles.icon} style={{top: '80px', left: '455px'}}>
-            <FontAwesomeIcon icon={faLaptop} />
-          </div>
-          <div className={styles.icon} style={{top: '420px', left: '75px'}}>
-            <FontAwesomeIcon icon={faLaptop} />
-          </div>
-          <div className={styles.icon} style={{top: '465px', left: '235px'}}>
-            <FontAwesomeIcon icon={faLaptop} />
-          </div>
-          <div className={styles.icon} style={{top: '465px', left: '455px'}}>
-            <FontAwesomeIcon icon={faLaptop} />
-          </div>
-        </div> */}
+          {devices && devices.map((device: any) => (
+            <div key={device.id} className={styles.marker} style={{ top: `${device.pos_x}%`, left: `${device.pos_y}%`}}>
+              <FontAwesomeIcon icon={deviceIcon(device.type_id)} />
+              {device.name}
+            </div>)
+            
+          )}
       </div>
     </div>
   )
