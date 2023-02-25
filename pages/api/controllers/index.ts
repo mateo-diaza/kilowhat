1218212e09ed9e08d9de368b06a446fac4b335ae
mateo <mateo.diaz@udc.es>
2023-02-25 01:15:12 +0100

    Añadir rpc a query local en la BBDD

--- pages/api/controllers/index.ts
+++ pages/api/controllers/index.ts
@@ -1,11 +1,36 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import supabase from '../utils/supabase'
 
+type CustomList = [
+  {
+    office: {
+      id: number,
+      name: string,
+      device: [
+        {
+          name: string,
+          emission: [
+            {
+              value: number,
+              sensor: [
+                {
+                  name: string,
+                  unit: string
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  }
+]
+
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
   ){
-    const result = await supabase.from('office').select('id, name')
+    let {data} = await supabase.rpc('hello_world')
     
-    res.status(200).json(result.data);
+    res.status(200).json(data);
 }
