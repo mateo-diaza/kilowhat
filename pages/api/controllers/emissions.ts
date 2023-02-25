7fa65bc8d5c958184bad37f0ab5b7b5ae5ea49ed
mateo <mateo.diaz@udc.es>
2023-02-25 03:55:18 +0100

    Añadido gráfico en targets

--- pages/api/controllers/emissions.ts
+++ pages/api/controllers/emissions.ts
@@ -5,17 +5,7 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
   ){
-    const result = await supabase
-      .from('device_type')
-      .select(`
-      id,
-      name,
-      device(
-        id,
-        name,
-        last_emissions(sum, created_at, device_id, sensors:sensor_id(name))
-      )
-    `);
+    const result = await supabase.from('emission').select('created_at, value');
     
     res.status(200).json(result.data);
 }
--- pages/targets/index.tsx
+++ pages/targets/index.tsx
@@ -1,7 +1,83 @@
 import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker'
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
 
-const Targets: React.FC = () => {
-    return (<> Targets </>)
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+const labels = [ 'Power', 'Temperature', 'Humidity'];
+
+export const data2 = {
+  labels,
+  datasets: [
+    {
+      fill: true,
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      fill: true,
+      label: 'PC',
+      data: [600, 200, 900, 300, 800, 500, 100],
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+    {
+        fill: true,
+        label: 'Dataset 3',
+        data: [400, 800, 100, 700, 200, 500, 900],
+        borderColor: 'rgb(178,34,34)',
+        backgroundColor: 'rgb(255,0,0)',
+    }
+  ],
+};
+
+
+interface IProps {
+  children?: JSX.Element
+}
+
+const Targets: React.FC<IProps> = () => {
+  return <Line options={options} data={data} />;
 }
 
 export default Targets;
