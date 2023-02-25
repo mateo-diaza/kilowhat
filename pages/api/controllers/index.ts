81c0e486bad1ec1eda88ba0c65f9e3698a35f7e5
Leopoldo Estevez <leopoldoestevezagra@gmail.com>
2023-02-25 02:04:33 +0100

    Added: Last temporal data emissions

--- pages/api/controllers/index.ts
+++ pages/api/controllers/index.ts
@@ -10,7 +10,22 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
   ){
-    const result = await supabase.from('office').select('id, name')
+    //const result = await supabase
+      //.from('emission')
+      //.select();
+    
+    // const result = await supabase.from('last_emissions').select('sum, created_at, device:device_id(name, office(name)))');
+    const result = await supabase
+      .from('device_type')
+      .select(`
+      id,
+      name,
+      device(
+        id,
+        name,
+        last_emissions(sum, created_at, device_id, sensors:sensor_id(name))
+      )
+    `);
     
     res.status(200).json(result.data);
 }
