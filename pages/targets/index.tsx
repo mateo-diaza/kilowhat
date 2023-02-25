8b7d07218cbc64b51df4bd922ab88cbf9d0a0a63
mateo <mateo.diaz@udc.es>
2023-02-25 18:21:26 +0100

    Added: Graphic buttons CSS and new default objective values

--- pages/targets/index.tsx
+++ pages/targets/index.tsx
@@ -1,11 +1,12 @@
+import styles from '@/styles/Targets.module.css';
 import EmissionGraphic from '@/components/EmissionGraphic';
 import { Colors } from 'chart.js';
 import React, { useState, useMemo, useEffect } from 'react';
 
 const Targets = () => {
 
-    const [ sensorId, setSensorId] = useState(2);
-    const [ objective, setObjective] = useState(0);
+    const [ sensorId, setSensorId] = useState(1);
+    const [ objective, setObjective] = useState(25);
     const [ graphicTitle, setGraphicTitle] = useState('Power');
     const [ lineColor, setLineColor] = useState('#FFD700');
 
@@ -13,29 +14,29 @@ const Targets = () => {
         setSensorId(1);
         setGraphicTitle('Power');
         setLineColor('#FFD700');
-        setObjective(23);
+        setObjective(25);
     }
 
     const handleTemperatureGraphic = () => {
         setSensorId(2);
         setGraphicTitle('Temperature');
         setLineColor('#FF8C00');
-        setObjective(17.5);
+        setObjective(22);
     }
 
     const handleHumidicityGraphic = () => {
         setSensorId(3);
         setGraphicTitle('Humidicity');
         setLineColor('#00BFFF');
-        setObjective(12.5);
+        setObjective(27);
     }
 
     return (
         <div>
             <div>
-                <button type='button' onClick={() => {handlePowerGraphic()}}>Power</button>
-                <button type='button' onClick={() => {handleTemperatureGraphic()}}>Temperature</button>
-                <button type='button' onClick={() => {handleHumidicityGraphic()}}>Humidicity</button>
+                <button type='button' className={styles.graphicButton} onClick={() => {handlePowerGraphic()}}>Power</button>
+                <button type='button' className={styles.graphicButton} onClick={() => {handleTemperatureGraphic()}}>Temperature</button>
+                <button type='button' className={styles.graphicButton} onClick={() => {handleHumidicityGraphic()}}>Humidicity</button>
             </div>
             <EmissionGraphic sensorId={sensorId} objective={objective} graphicTitle={graphicTitle} lineColor={lineColor} />
         </div>
